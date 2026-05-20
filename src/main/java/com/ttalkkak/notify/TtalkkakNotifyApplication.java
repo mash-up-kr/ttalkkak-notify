234f3f687e9fa3768f583d90a4bf5e27acd515cb
mingdodev <mingdo.dev@gmail.com>
2026-05-20 21:37:52 +0900

    프로젝트 생성

---
+++ src/main/java/com/ttalkkak/notify/TtalkkakNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.ttalkkak.notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtalkkakNotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtalkkakNotifyApplication.class, args);
+    }
+
+}
